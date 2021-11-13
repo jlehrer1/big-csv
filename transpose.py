da0226a588b774aec13ebefe1b712af27e8dc55a
Julian Lehrer <jmlehrer@ucsc.edu>
2021-11-13 12:33:37 -0800

    Updated gitignore, cleaned up script

--- transpose.py
+++ transpose.py
@@ -1,21 +1,39 @@
-from genericpath import exists
 import pandas as pd 
 import numpy as np 
 import os 
 import argparse
 import pathlib
 import subprocess
-import boto3
-from upload import upload 
+import boto3 
+
+from upload_helper import upload 
 
 here = pathlib.Path(__file__).parent.resolve()
 
 parser = argparse.ArgumentParser(description='Calculate the transpose of a large file')
-parser.add_argument('-file', metavar='F', type=str, help='Path to the input file')
-parser.add_argument('-chunksize', type=int, help='Chunksize to use, equivalently, number of rows to read into memory at once.')
-parser.add_argument('-sep', type=str, help='File seperator. Should be either \',\' for csv or \'\\t\' for tsv')
+parser.add_argument(
+    '-file', 
+    type=str, 
+    help='Path to the input file', 
+    required=True
+)
+
+parser.add_argument(
+    '-chunksize', 
+    type=int, 
+    help='Chunksize to use, equivalently, number of rows to read into memory at once.', 
+    required=True
+)
+
+parser.add_argument(
+    '-sep', 
+    type=str,
+    help='File seperator. Should be either \',\' for csv or \'\\t\' for tsv',
+    required=True
+)
 
 args = parser.parse_args()
+
 file = args.file
 chunksize = args.chunksize 
 sep = args.sep
@@ -35,9 +53,12 @@ chunksizes = []
 
 for df, l in zip(pd.read_csv(file, sep='\t', chunksize=chunksize), range(0, lines // chunksize + int(lines % chunksize == 0))):  # if we have one last small chunk or not 
     print(f'Working on chunk {l}')
+
     print(f'Chunk {l} has shape {df.shape}')
     df = df.T
+
     print(f'Writing chunk {l} to csv')
-    df.to_csv(os.path.join(here, 'chunks', f'{l}.csv'), sep=',', index=False)
+    df.to_csv(os.path.join(here, 'chunks', f'{l}_{file[:-4]}.csv'), sep=',', index=False)
+
     print(f'Uploading chunk {l} to S3')
-    upload(os.path.join(here, 'chunks', f'{l}.csv'))
+    upload(os.path.join(here, 'chunks', f'{l}_{file[:-4]}.csv'), f'{l}_{file[:-4]}.csv')
