fdf1bfcd0682e3a71ba7b3e3e42912b9da5ab381
Julian Lehrer <jmlehrer@ucsc.edu>
2022-02-15 12:24:58 -0800

    Cleanup

--- combine.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-paste -d ',' chunks/* > ${1}.csv # Join all files horizontally
-tail -n +2 ${1}.csv > ${1}_clipped.csv # Remove first line since it extraneous
-mv ${1}_clipped.csv ${1}.csv # Move that fixed file back to the original passed file name 
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export FILE=organoid
-envsubst < run.yaml | kubectl create -f -
-
-export FILE=primary
-envsubst < run.yaml | kubectl create -f -
-
