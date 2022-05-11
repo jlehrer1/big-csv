57ca11722181dedbbbb3184c130a1c11f27cd15a
Julian Lehrer <jmlehrer@ucsc.edu>
2022-05-10 19:18:32 -0700

    Bugfixc

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jlehrer1/big-csv',
-    version='1.0.1',
+    version='1.0.2',
     zip_safe=False,
 )
